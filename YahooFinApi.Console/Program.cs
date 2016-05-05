4c9c09fe51e99c90a0b219afd97acec167e05306
Vaibhav Bali <vbali@internode.on.net>
2016-05-05 16:08:26 +1000

    init

---
+++ YahooFinApi.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using YahooFinApi.HttpServer;
+
+namespace YahooFinApi.Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new QuotesRetriever();
+
+            System.Console.WriteLine(client.DownloadData("http://real-chart.finance.yahoo.com/table.csv?s=BHP.AX&d=1&e=29&f=2016&g=d&a=0&b=29&c=1988&ignore=.csv").Result);
+        }
+    }
+}
---
+++ YahooFinApi.HttpServer/QuotesRetriever.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+using System.Threading.Tasks;
+
+namespace YahooFinApi.HttpServer
+{
+    public class QuotesRetriever
+    {
+        private readonly WebClient client;
+        public QuotesRetriever()
+        {
+            client = new WebClient();
+        }
+
+        public async Task<string> DownloadData(string query)
+        {
+            var downloadTask = Task.Run(() => client.DownloadString(query));
+
+            await downloadTask;
+            return downloadTask.Result;
+        }
+    }
+}
