c328817928872324a5f117fcbe946de33bed5c6a
Vaibhav Bali <vibsbali@gmail.com>
2016-10-13 10:34:59 +1100

    Added C$ interpolation

--- YahooFinApi.Console/Program.cs
+++ YahooFinApi.Console/Program.cs
@@ -7,9 +7,9 @@ namespace YahooFinApi.Console
         static void Main()
         {
             var client = new QuotesRetriever();
-
+            var stock = "BHP.AX";
             //We can use C#6 interpolation to pass various parameters such as BHP.AX, date and so forth
-            System.Console.WriteLine(client.DownloadDataAsync("http://real-chart.finance.yahoo.com/table.csv?s=BHP.AX&d=1&e=29&f=2016&g=d&a=0&b=29&c=1988&ignore=.csv").Result);
+            System.Console.WriteLine(client.DownloadDataAsync($"http://real-chart.finance.yahoo.com/table.csv?s={stock}&d=1&e=29&f=2016&g=d&a=0&b=29&c=1988&ignore=.csv").Result);
         }
     }
 }
