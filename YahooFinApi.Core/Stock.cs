62dbb189212215f6192913b9449b165fa9d6e3e1
Vaibhav Bali <vibsbali@gmail.com>
2016-10-13 10:06:12 +1100

    Updated App.config to enable/disable proxy

--- YahooFinApi.Core/Stock.cs
+++ YahooFinApi.Core/Stock.cs
@@ -1,8 +1,16 @@
-﻿namespace YahooFinApi.Core
+﻿using System;
+
+namespace YahooFinApi.Core
 {
     public class Stock
     {
         public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public uint Volume { get; set; }
+        public decimal OpeningPrice  { get; set; }
         public decimal ClosingPrice { get; set; }
+        public decimal AdjustedClose { get; set; }
     }
 }
